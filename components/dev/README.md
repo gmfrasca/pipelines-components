502ac87da9df27f75aa0759da5b44563345853fc
Giulio Frasca <gfrasca@redhat.com>
2025-12-03 03:11:04 -0500

    feat(components): Add a basic example hello world component
    Signed-off-by: Giulio Frasca <gfrasca@redhat.com>

---
+++ components/dev/README.md
@@ -0,0 +1,12 @@
+# Dev Components
+
+This directory contains components in the **Dev** category.
+
+## Components
+
+### Hello World
+
+A simple hello world component.
+
+[View Details](./hello_world/README.md)
+
---
+++ components/dev/hello_world/__init__.py
@@ -0,0 +1,9 @@
+"""Training Components Module
+
+This module re-exports all components in the training category for easy import:
+    from kubeflow.pipelines.components.training import component_name
+"""
+
+# Auto-generated imports will be added here by scripts/update_init_imports.py
+# Components will be imported dynamically based on subdirectories
+from .component import hello_world
